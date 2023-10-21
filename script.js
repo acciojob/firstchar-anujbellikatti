dcd243ea99ad0e891bbbafe38da26b3dce2bcea0
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-10-21 16:24:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 function firstChar(text) {
   // your code here
 	let x = text.split('')
+	if(x[0]==" "){
+		return x[1].toString();
+	}else if(x.length==1&&x[0]==" "){
+		return " "
+	}
+	else{
 	return x[0].toString();
+		
+	}
+	
 }
 // Do not change the code below
 
 const text = prompt("Enter text:");
 alert(firstChar(text));
+
+
